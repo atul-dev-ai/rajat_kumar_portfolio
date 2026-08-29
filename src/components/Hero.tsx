27e664ae90365e3eee6bd2601fd3915e6eece0a2
atul-dev-ai <paulatul020@gmail.com>
2026-08-29 23:51:13 +0600

    feat: add Bootslander style floating animated waves to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -99,6 +99,18 @@ export function Hero() {
           </motion.div>
         </div>
       </div>
+
+      {/* Floating Animated Waves */}
+      <svg className="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none">
+        <defs>
+          <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
+        </defs>
+        <g className="wave-group">
+          <use href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)" className="wave1"></use>
+          <use href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)" className="wave2"></use>
+          <use href="#wave-path" x="50" y="9" fill="#fff" className="wave3"></use>
+        </g>
+      </svg>
     </section>
   );
 }
